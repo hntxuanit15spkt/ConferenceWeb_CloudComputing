0490e4991eed87d5893714b245fd3d8036e91b9b
hntxuanit15spkt <hntxuanit15spkt@gmail.com>
2018-03-17 13:17:47 +0700

    Create model News

--- src/main/java/conferenceWeb/model/News.java
+++ src/main/java/conferenceWeb/model/News.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class News implements Serializable {
+	
 	private int id;
 	
 	private int conference_id;
@@ -17,6 +18,78 @@ public class News implements Serializable {
 	private Date create_time;
 	
 	private String image;
+
+	public News() {
+		
+	}
+
+	public News(int id, int conference_id, int account_id, String title, String content, Date create_time,
+			String image) {
+		super();
+		this.id = id;
+		this.conference_id = conference_id;
+		this.account_id = account_id;
+		this.title = title;
+		this.content = content;
+		this.create_time = create_time;
+		this.image = image;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getConference_id() {
+		return conference_id;
+	}
+
+	public void setConference_id(int conference_id) {
+		this.conference_id = conference_id;
+	}
+
+	public int getAccount_id() {
+		return account_id;
+	}
+
+	public void setAccount_id(int account_id) {
+		this.account_id = account_id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getCreate_time() {
+		return create_time;
+	}
+
+	public void setCreate_time(Date create_time) {
+		this.create_time = create_time;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
 	
 	
 }
